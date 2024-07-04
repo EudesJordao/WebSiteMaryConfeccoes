827ce781a95c0633910771dd39487f814e522ef5
Eudes Jordão <eudesjrd2015@gmail.com>
2024-07-03 23:48:27 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogoComponent } from './screens/catalogo/catalogo.component';
+import { HomeComponent } from './screens/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'catalogo', component: CatalogoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './screens/nav/nav.component';
+import { CatalogoComponent } from './screens/catalogo/catalogo.component';
+import { HomeComponent } from './screens/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CatalogoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
